e5d7705dbabd7e6827057d8a30be44d0ca55c6d6
Jan-Henrik Bruhn <hi@jhbruhn.de>
2020-05-10 14:53:50 +0200

    Enable low efficiency implementation for saturation functions

--- dsp/dsp.h
+++ dsp/dsp.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -108,7 +108,7 @@ inline float SoftClip(float x) {
   }
 }
 
-#ifdef TEST
+#if defined(TEST) || defined(STM32F0XX)
   inline int32_t Clip16(int32_t x) {
     if (x < -32768) {
       return -32768;
@@ -139,7 +139,7 @@ inline float SoftClip(float x) {
     return result;
   }
 #endif
-  
+
 #ifdef TEST
   inline float Sqrt(float x) {
     return sqrtf(x);
